ceaf6f1c044ee8ed400d6c762896b5f3e9c8f0b2
asim ali <asimalizamurani@gmail.com>
2024-09-18 12:40:48 +0500

    I started learning c# where i learnt the basics and also i created a simple calculator

---
+++ calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace conditional_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            string sign;
+            int sum;
+            Console.WriteLine("Enter your first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the sign");
+            sign = Console.ReadLine();
+
+            Console.WriteLine("Enter your second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            if(sign == "+")
+            {
+                sum = num1 + num2;
+                
+                Console.WriteLine($"The sum is : {sum}");
+            }
+            else if(sign == "-")
+            {
+                sum = num1 - num2;
+                if (sum < 0)
+                {
+                    Console.WriteLine("Negative values not allowed");
+                }
+                else
+                {
+                    Console.WriteLine($"The substraction is : {sum}");
+                }
+            }
+            else if(sign == "*")
+            {
+                sum = num1 * num2;
+                Console.WriteLine($"The multiplication is : {sum}");
+            }
+            else if(sign == "/")
+            {
+                sum = num1 / num2;
+                Console.WriteLine($"The division is : {sum}");
+            }
+            else if(sign == "%")
+            {
+                sum = num1 % num2;
+                Console.WriteLine($"The reminder is : {sum}");
+            }
+            
+            else
+            {
+                Console.WriteLine("Sorry bro");
+            }
+        }
+    }
+}
---
+++ project1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            int sum;
+
+            string fname, lname;
+
+            Console.WriteLine("Enter the First Number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            sum = num1 + num2;
+            Console.WriteLine($"The sum is : {sum}");
+
+            Console.WriteLine("Enter your first name");
+            fname = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            lname = Console.ReadLine();
+
+            Console.WriteLine($"Your Full Name is {fname} {lname}");
+
+            Console.WriteLine($"Hey Mr/Miss {fname} How are you doing");
+        }
+    }
+}

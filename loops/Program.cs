78057e6ae8d9a6bc5d493ec42e9e43183173b2e9
asim ali <asimalizamurani@gmail.com>
2024-09-23 12:18:11 +0500

    loops in csarp

---
+++ loops/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace demoproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loops");
+            // For Loop
+            /*
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            // While Loop
+            /*
+            int b = 1;
+            while (b < 10)
+            {
+                Console.WriteLine(b);
+                b++;
+            }
+            */
+
+            /* Do While Loop */
+            /* Console.WriteLine("Do While Loop");*/
+            /*int c = 1;
+            do
+            {
+                Console.WriteLine(c);
+
+                c++;
+
+            } while (c < 10);*/
+
+
+            /* Multiplication Table */
+            int n;
+
+            Console.WriteLine("Enter a number to print its table");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} * {i} = {n * i}");
+            }
+
+
+        }
+    }
+}
